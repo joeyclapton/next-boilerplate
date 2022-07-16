a30443b09859d0573f08202bc11152b8dcce920f
joeyclapton <joeyclapton42@gmail.com>
2022-07-16 08:08:50 -0300

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+/* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+
+// eslint-disable-next-line no-undef
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode: true
 }
 
-module.exports = nextConfig
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  },
+  nextConfig
+})
